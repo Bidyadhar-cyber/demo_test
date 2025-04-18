213087c364397d6d44398e79f5b6405fc10709cc
Bidyadhar-cyber <bidyakhd@gmail.com>
2025-04-18 09:40:49 +0530

    Update setup.py

--- embedded_config/setup.py
+++ embedded_config/setup.py
@@ -1,5 +1,10 @@
 from setuptools import setup, find_packages
+import os
 
+if os.path.exists("README.md"):
+    long_desc = open("README.md").read()
+else:
+    long_desc = "Default description"
 setup(
     name="embedded_config",
     version="1.0",
@@ -9,7 +14,7 @@ setup(
     ],
     author="Bidyadhar Muduli",
     description="A package for embedded system input, output, and PI info",
-    long_description=open("README.md").read(),
+    long_description= long_desc,
     long_description_content_type="text/markdown",
     classifiers=[
         "Programming Language :: Python :: 3",
